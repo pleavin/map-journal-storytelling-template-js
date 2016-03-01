d32eaf3d0c644dc9d1cb29bffb99bb80c329621e
pleavin <victoria.pleavin@gmail.com>
2016-02-29 17:29:54 -0700

    end of day commit

---
+++ src/app/storymaps/tpl/ui/desktop/SquareNavBar.js
@@ -0,0 +1,17 @@
+
+
+<div class="square0">
+  <a href="#">.</a>
+</div>
+<div class="square1">
+  <a href="#">.</a>
+</div>
+<div class="square2">
+  <a href="#">.</a>
+</div>
+<div class="square3">
+  <a href="#">.</a>
+</div>
+<div class="square4">
+  <a href="#">.</a>
+</div>
